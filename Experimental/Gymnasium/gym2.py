10d22b545e9f6725c263b983abeb0b066550d509
Virtual IBN <73728839+IBN5101@users.noreply.github.com>
2023-02-16 00:51:03 +0000

    Gymnasium progress #01
    Next: Extends ant_v4 or mujoco_env to create a new environment.

---
+++ Experimental/Gymnasium/gym2.py
@@ -0,0 +1,28 @@
+import gymnasium as gym
+
+import sys
+import traceback
+
+path = "/home/vboxuser/Desktop/HQplus/CS3IP/blobby.xml"
+
+try:
+    env = gym.make("Ant-v4", render_mode="human", xml_file=path)
+
+    observation, info = env.reset(seed=42)
+    for _ in range(1000):
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
+except:
+    _, _, tb = sys.exc_info()
+    traceback.print_tb(tb)
+    tb_info = traceback.extract_tb(tb)
+    filename, line, func, text = tb_info[-1]
+
+    print('Lin {} in statement {}'.format(line, text))
+
+
+
